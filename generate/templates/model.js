b7d6311926b34f70e7228c2ca12b69e7a83f3143
Jim Snodgrass <jim@skookum.com>
2012-03-06 12:18:42 -0500

    generate controller, model, and test for a resource
    still needs to update evn file with mongoose data and make sure mongoose is in package.json

--- generate/templates/model.js
+++ generate/templates/model.js
@@ -4,9 +4,7 @@
 
 var mongoose = require('mongoose');
 
-var _resource_ = new mongoose.Schema(
-  _props_, 
-  {strict: true});   // turn off strict if you would like to store data that isn't in schema
+var _resource_ = new mongoose.Schema(_props_, {strict: true});   // turn off strict if you would like to store data that isn't in schema
 
 
 // Export
